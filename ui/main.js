3206166cc2e8618c59f595de5377bb77d5005f8b
yash627 <yash9276@gmail.com>
2016-10-07 21:49:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,22 @@
 var button=document.getElementById('counter');
-var counter =0;
 button.onclick=function()
 {
     
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function()
+    {
+        if (request.readyState===XMLHttpRequest.Done)
+        {
+            if (request.status===200)
+            {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
     
-    
-    counter=counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+   request.open ('GET', 'http://yash627.imad.hasura-app.io/counter',true);
+   request.send (null);
+   
 };
