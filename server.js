69b67cf71612f7a1e4d31b1560a9f94339e48d73
yash627 <yash9276@gmail.com>
2016-10-08 17:02:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,7 +34,18 @@ var articles={
                     </p>
                     <hr/>
                     <h3> Gyan Bharati School </h3>
-                    <hr/>`
+                    <hr/>
+                    <br/>
+                    <h3>Courses<h3/>
+                    <ol>
+                    <li>AP 101 <li/>
+                    <li>MA 101 <li/>
+                    <li>HU 101 <li/>
+                    <li>ME 101 <li/>
+                    <li>ME 103 <li/>
+                    <li>AC 101 <li/>
+                    
+                    `
                     
                     
                     
