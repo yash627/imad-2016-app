1201e4b6d0c04bc75aead19031646db69a4c8c30
yash627 <yash9276@gmail.com>
2016-10-08 19:19:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ var articles={
                     <h3> Delhi Technological University </h3>
                     <p>
                     Bachelor of Technology (B.Tech.), Civil Engineering, (2016-2020)
-                     <img src="https://upload.wikimedia.org/wikipedia/en/b/b5/DTU%2C_Delhi_official_logo.png" class="img-small"/>
                      </p>
+                     <img src="https://upload.wikimedia.org/wikipedia/en/b/b5/DTU%2C_Delhi_official_logo.png" class="img-small"/>
                     <hr/>
                     <h3> Gyan Bharati School,New Delhi </h3>
                     <hr/>
