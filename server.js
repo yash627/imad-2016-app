ae37e8e49c14666ad24d0e0fa2269898e806bfcb
yash627 <yash9276@gmail.com>
2016-11-12 18:31:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,9 +21,10 @@ var articles={
                         `
                                                 },
     'article-two' : { title : 'My Education | Yash Khurana',
-        heading : 'Education` `<p> img src= "http://globalgurus.org/Blog/wp-content/uploads/2015/08/education-11.jpg" class="img-s.small"</p>',
+        heading : 'Education',
        
         content : ` 
+                     <img src= "http://globalgurus.org/Blog/wp-content/uploads/2015/08/education-11.jpg" class="img-s.small">
                     <hr/>
                     <h3> Delhi Technological University </h3>
                     <p>
