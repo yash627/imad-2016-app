93494b4c2b42ad226ae93b7837f4cf5496ead0bf
yash627 <yash9276@gmail.com>
2016-11-14 16:11:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+	user: 'yash627',
+	database: 'yash627',
+	host: 'db.imad.hasura-app.io',
+	port: '5432',
+	password: process.env.DB_PASSWORD
+	         }
 
 var app = express();
 app.use(morgan('combined'));
@@ -93,6 +102,16 @@ return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res) {
+	pool.query("select * from test", function (err, result) {
+    	if (err)
+			res.status(500).send(err.toString());
+		else    
+			res.send(JSON.stringify(result.rows));
+	});
 });
 
 var counter=0;
