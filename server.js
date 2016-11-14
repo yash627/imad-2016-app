42952c584457a8066d2676153f8c2aa13598120d
yash627 <yash9276@gmail.com>
2016-11-14 23:22:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -142,9 +142,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/info.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'info.js'));
-});
+app.get('/ui/info.js', function (req, res) { 
+  res.sendFile(path.join(__dirname, 'ui', 'info.js')); 
+}); 
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
