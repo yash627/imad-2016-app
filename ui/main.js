3a4c988261b5a45e27a18175c29dcc5355320ed0
yash627 <yash9276@gmail.com>
2016-11-15 15:52:01 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,122 +1,114 @@
-var submit = document.getElementById('submit_btn');
-submit.onclick= function(){
+var register_btn = document.getElementById('register_btn');
+var login_btn = document.getElementById('login_btn');
+var logout_btn = document.getElementById('logout_btn');
+var username = document.getElementById('username');
+var password = document.getElementById('password');
+var new_username = document.getElementById('new_username');
+var new_password = document.getElementById('new_password');
 
-     var request = new XMLHttpRequest(); 
-  request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE){
-         if(request.status === 200){
-             
-             console.log('user loged in');
-             alert("Login Successfully");
-             window.location.href ='/info';
-         }else {
-             if(request.status==403){
-                 alert("Invalid password/Username");
-                 submit.value = 'Invalid credentials. Try again!';
-             }else {
-             if(request.status==500){
-                 alert("Internal Server Error");
-             }
-          }
-         }
-     }
-     
- };
- 
- var username = document.getElementById('username').value;
- var password = document.getElementById('password').value;
- console.log(username);
- console.log(password);
- request.open('POST','http://yash627.imad.hasura-app.io/login', true);
- request.setRequestHeader('Content-Type','application/json');
- request.send(JSON.stringify({username: username, password: password}));  
- submit.value = 'Logging in...'; 
-   
-};
-
-
- var register = document.getElementById('register_btn');
- register.onclick = function () {
-     
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-
-          if (request.readyState === XMLHttpRequest.DONE) {
-              if (request.status === 200) {
-                  alert('User created successfully');
-                  register.value = 'Registered!';
-              } else {
-                  alert('Could not register the user');
-                  register.value = 'Register';
-              }
-
-          }
-
-        };
-        
-      var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        request.open('POST', '/create-user', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        register.value = 'Registering...';
-
-     
- };
- 
- function loadLogin () {
-
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                loadLoggedInUser(this.responseText);
-            } else {
-                loadLoginForm();
-            }
-        }
-    };
-
-    
-
-    request.open('GET', '/check-login', true);
-
-    request.send(null);
-
+if (login_btn) {
+	login_btn.onclick = function(){
+			username = document.getElementById('username').value;
+		password = document.getElementById('password').value;
+		if (username === '' || password === '') {
+   					$("#username").attr("placeholder", "Required !");
+   					$("#password").attr("placeholder", "Required !");
+   				} else {
+					var req = new XMLHttpRequest();	
+					req.onreadystatechange = function(){
+						if(req.readyState === XMLHttpRequest.DONE){
+							// Do something
+							if (req.status === 200){
+								console.log('user logged in');
+								console.log(username);
+								console.log(password);
+								checklogin();
+							} else if (req.status === 403) {
+								alert('username/password is incorrect or does not exist!');
+							} else if (req.status === 500) {
+								alert('username does not exist!');
+							}
+						}
+					};
+		
+		
+		req.open("POST", window.location.protocol+"//"+window.location.host+"/login", true);
+		req.setRequestHeader('Content-Type', 'application/json');
+		req.send(JSON.stringify({username: username, password: password}));
+		}
+	}
 }
 
 
-function loadArticles () {
-    
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            var articles = document.getElementById('articles');
-            if (request.status === 200) {
-                var content = '<ul>';
-                var articleData = JSON.parse(this.responseText);
-                for (var i=0; i< articleData.length; i++) {
-   content += `<li> <a  href="/articles/${articleData[i].title}">${articleData[i].heading}</a>(${articleData[i].date.split('T')[0]})</li>`;
-                }
-                content += "</ul>";
-                articles.innerHTML = content;
-            } else {
-               articles.innerHTML('Oops! Could not load all articles!');
-            }
-        }
-    };
 
-    request.open('GET', '/get-articles', true);
-    request.send(null);
+if (register_btn) {
+	register_btn.onclick = function(){
+			new_username = document.getElementById('new_username').value;
+		new_password = document.getElementById('new_password').value;
 
+		var req = new XMLHttpRequest();
+		var req2 = new XMLHttpRequest();
+		if (new_username === '' || new_password === '') {
+   					$("#new_username").attr("placeholder", "Required !");
+   					$("#new_password").attr("placeholder", "Required !");
+   				} else {	
+				req.onreadystatechange = function(){
+					if(req.readyState === XMLHttpRequest.DONE){
+				// Do something
+				if (req.status === 200){
+							document.getElementById('username').value = document.getElementById('new_username').value;
+							document.getElementById('password').value = document.getElementById('new_password').value;
+							document.getElementById('new_username').value = '';
+							document.getElementById('new_password').value = '';
+							alert('New user created!');
+							$('#login_btn').click();
+				} else if (req.status === 403) {
+					alert('username/password is incorrect');
+				} else if (req.status === 500) {
+					console.log('username = '+new_username);
+					console.log('password = '+new_password);
+					alert('username already exists!');
+				}
+			}
+		};
+			console.log(new_username);
+		console.log(new_password);
+		req.open("POST", window.location.protocol+"//"+window.location.host+"/create-user", true);
+		req.setRequestHeader('Content-Type', 'application/json');
+		req.send(JSON.stringify({username: new_username, password: new_password}));
+	}
+		
+	}
 }
 
 
 
 
-loadArticles();
+
+
+if (logout_btn) {
+	$('#logout_btn')[0].onclick = logout;
+	checklogin();
+}
+
+
+function logout(){
+	var req = new XMLHttpRequest();	
+		req.onreadystatechange = function(){
+			if(req.readyState === XMLHttpRequest.DONE){
+				// Do something
+				if (req.status === 200){
+					console.log('user logged out');
+					checklogin();
+				} else if (req.status === 403) {
+					alert('username/password is incorrect!');
+				} else if (req.status === 500) {
+					alert('username/password is incorrect!');
+				}
+			}
+		};
+		req.open("GET", window.location.protocol+"//"+window.location.host+"/logout", true);
+		req.send(null);
+
 
        
