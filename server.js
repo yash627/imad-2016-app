3f6d0cebcdd240aae94475a8692c68fe751dba7a
yash627 <yash9276@gmail.com>
2016-11-17 22:51:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -80,41 +80,35 @@ function createTemplate (data) {
  var heading = data.heading;
  var content = data.content;
 
-var htmlTemplate = `
-    <html>
-      <head>
-          <title>
-              ${title}
-          </title>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link href="/ui/style.css" rel="stylesheet" />
-      </head> 
-      <body>
-          <div class="container">
-              <div>
-                  <a href="/">Home</a>
-              </div>
-              <hr/>
-              <h3>
-                  ${heading}
-              </h3>
-              <div>
-                ${content}
-              </div>
-              <hr/>
-              <h4>Comments</h4>
-              <div id="comment_form">
-              </div>
-              <div id="comments">
-                <center>Loading comments...</center>
-              </div>
-          </div>
-          <script type="text/javascript" src="/ui/article.js"></script>
-      </body>
-    </html>
-    `;
-    return htmlTemplate;
-}
+var htmlTemplate =`		
+<html>		 
+<head>		
+    <title>		 
+       ${title}		
+    </title>		
+    <meta name="viewport" content ="width-device-width, initial-scale=1 "/>		
+    <link href="/ui/style.css" rel="stylesheet" />		
+    		
+</head>    		
+       <body>		
+         <div class= "container">		
+             <div>		 
+                 <a href="/">Home</a>		 
+             </div>		
+           <hr/>		
+            <h3>		
+                ${heading}		
+            </h3>		
+          		
+            <div>		 
+               ${content}		 
+            </div>		
+        </div>		
+       </body>		
+     		
+ </html>		
+ `;		
+ return htmlTemplate;
 
 app.get('/article-one', function (req, res) {
    res.send(createTemplate(article-one));
