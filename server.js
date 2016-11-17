ec131b8a0de778453059dc9f44d5384d7e0163d4
yash627 <yash9276@gmail.com>
2016-11-17 21:28:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,13 +117,6 @@ app.get('/test-db', function(req, res) {
 	});
 });
 
-var counter=0;
-app.get('/counter',function(req,res)
-{
-    counter=counter+1;
-    res.send(counter.toString());
-});
-
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
